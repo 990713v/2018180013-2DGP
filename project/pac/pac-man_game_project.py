bccbfab36b0b41a3c1e8a77b8584fc67f6311cf8
2018180013-2DGP <1057542@naver.com>
2019-10-03 05:18:33 +0900

    게임개발
    리소스 배경 편집
    키보드 입력 처리

--- project/pac/pac-man_game_project.py
+++ project/pac/pac-man_game_project.py
@@ -3,52 +3,71 @@
 # 맵 배열 생성 (벽과 길 구분, 벽은 1 길은 0으로)
 
 from pico2d import *
-
 open_canvas()
 
-map = load_image('original_map.png')
-emy1 = load_image('red.png') # 너비 테스트 29*16
+def handle_events():
+    global nowX
+    global nowY
+    global locate
+    events = get_events()
+
+    for event in events:
+        if event.type == SDL_KEYDOWN:
+                if event.key == SDLK_RIGHT:
+                   nowX += 1
+                   locate = 1
+                elif event.key == SDLK_LEFT:
+                   nowX -= 1
+                   locate = 2
+                elif event.key == SDLK_UP:
+                   nowY += 1
+                   locate = 3
+                elif event.key == SDLK_DOWN:
+                   nowY -= 1
+                   locate = 4
+
+        elif event.type == SDL_KEYUP:
+                if event.key == SDLK_RIGHT:
+                   nowX -= 1
+                elif event.key == SDLK_LEFT:
+                   nowX += 1
+                elif event.key == SDLK_UP:
+                   nowY -= 1
+                elif event.key == SDLK_DOWN:
+                   nowY += 1
+
+map = load_image('coin_map.png')
+emy1 = load_image('blue.png')  # 너비 테스트 29*16
 player = load_image('pacman.png')
-#리소스 테스트
+# 리소스 테스트
 player2 = load_image('pac_die.png')
 
-x= 280
-x2 = 280
-y = 300
-y2 = 300
+x = 400
+y = 240
+
 frame = 5
-n = 0
+nowX = 0
+nowY = 0
+locate = 0
 
 while 1:
-        while n < 15:
-                clear_canvas()
-                map.draw(400, 300) # 맵 띄우기
-                player.clip_draw(frame * 29, 48, 29, 16, 280, y) # 팩맨 위
-                player.clip_draw(frame * 29, 32, 29, 16, 280, y2) # 팩맨 아
-                player.clip_draw(frame * 29, 16, 29, 16, x2, 260) # 팩맨 왼
-                player.clip_draw(frame * 29, 0, 29, 16, x, 260) # 팩맨 오
+        clear_canvas()
+        map.draw(400, 300)  # 맵 띄우기
 
-                update_canvas()
-                frame = (frame+1)%8
-                delay(0.08)
-        
-                x += 1
-                y += 1
-                x2 -= 1
-                y2 -= 1
-                n += 1
-        
-                get_events()
-                
-        while n >= 0:
-                clear_canvas()
-                map.draw(400, 300) # 맵 띄우기
-                player2.clip_draw(frame * 16, 0, 16, 16, 320, 300)
-                update_canvas()
-                frame = (frame+1)%8
-                delay(0.15)
-                n -=0.2
-        
+        handle_events()
+        if locate <= 1: # 오른쪽
+            player.clip_draw(frame * 29, 0, 29 ,16, x, y)
+        elif locate == 2: # 왼쪽
+            player.clip_draw(frame * 29, 16, 29, 16, x, y)
+        elif locate == 3: #위쪽
+            player.clip_draw(frame * 29, 48, 29, 16, x, y)
+        elif locate == 4: #아래쪽
+            player.clip_draw(frame * 29, 32, 29, 16, x, y)
+        update_canvas()
+
+        frame = (frame + 1) % 8
+        x += nowX * 5
+        y += nowY * 5
+        delay(0.1)
 
 close_canvas()
-n
