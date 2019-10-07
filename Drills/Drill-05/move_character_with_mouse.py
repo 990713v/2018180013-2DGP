a97a1301052916880945cad178993ca4f1d34e9a
2018180013-2DGP <1057542@naver.com>
2019-10-07 18:47:15 +0900

    Drill-05
    캐릭터 마우스 이동 추가

--- Drills/Drill-05/move_character_with_mouse.py
+++ Drills/Drill-05/move_character_with_mouse.py
@@ -5,11 +5,15 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 def handle_events():
     global running
     global x, y
+    global nowX, nowY  # 캐릭터 좌표
+
     events = get_events()
 
     for event in events:
         if event.type == SDL_QUIT:
             running = False
+        if event.type == SDL_MOUSEBUTTONDOWN: #버튼 눌렀을 때
+            nowX, nowY = x, y
         elif event.type == SDL_MOUSEMOTION: #마우스 모션이벤트 감지
             x, y = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
@@ -25,15 +29,16 @@ running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
 hide_cursor() # 마우스 커서 숨김. show_cursor() 는 커서 아이콘 출력
-
+nowX, nowY = x, y
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     pointer.draw(x, y)
+    character.clip_draw(frame*100,0,100,100, nowX, nowY)
     update_canvas()
     frame = (frame + 1) % 8
 
-    #delay(0.02)
+    delay(0.02)
     handle_events()
 
 close_canvas()
