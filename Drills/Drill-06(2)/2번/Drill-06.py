50cf82142ea51debc591d9a09b659a3aca1b2caa
2018180013-2DGP <1057542@naver.com>
2019-10-09 19:29:19 +0900

    Update Drill-06.py

--- Drills/Drill-06(2)/2번/Drill-06.py
+++ Drills/Drill-06(2)/2번/Drill-06.py
@@ -20,7 +20,7 @@ global Doit
 
 def handle_events():
     global running
-    global end_x, end_y
+    global endX, endY
     global nowX, nowY
 
     events = get_events()
@@ -28,39 +28,36 @@ def handle_events():
     for event in events:
         if event.type == SDL_MOUSEBUTTONDOWN:
             for i in range(0, 100, 2):
-                # draw p1-p2
-                for i in range(0, 100, 2):
-                    t = i / 100
-                    nowX = ((-t ** 3 + 2 * t ** 2 - t) * point[1][0] + (3 * t ** 3 - 5 * t ** 2 + 2) * point[1][0]
+                t = i / 100
+                end_x = ((-t ** 3 + 2 * t ** 2 - t) * point[1][0] + (3 * t ** 3 - 5 * t ** 2 + 2) * point[1][0]
                          + (-3 * t ** 3 + 4 * t ** 2 + t) * point[2][0] + (t ** 3 - t ** 2) * point[3][0]) / 2
-                    nowY = ((-t ** 3 + 2 * t ** 2 - t) * point[4][1] + (3 * t ** 3 - 5 * t ** 2 + 2) * point[1][1]
+                nowY = ((-t ** 3 + 2 * t ** 2 - t) * point[4][1] + (3 * t ** 3 - 5 * t ** 2 + 2) * point[1][1]
                          + (-3 * t ** 3 + 4 * t ** 2 + t) * point[2][1] + (t ** 3 - t ** 2) * point[3][1]) / 2
 
-                # draw p2-p3
-                for i in range(0, 100, 2):
-                    t = i / 100
-                    nowX = ((-t ** 3 + 2 * t ** 2 - t) * point[1][0] + (3 * t ** 3 - 5 * t ** 2 + 2) * point[2][0]
+            # draw p2-p3
+            for i in range(0, 100, 2):
+                t = i / 100
+                nowX = ((-t ** 3 + 2 * t ** 2 - t) * point[1][0] + (3 * t ** 3 - 5 * t ** 2 + 2) * point[2][0]
                          + (-3 * t ** 3 + 4 * t ** 2 + t) * point[3][0] + (t ** 3 - t ** 2) * point[4][0]) / 2
-                    nowY = ((-t ** 3 + 2 * t ** 2 - t) * point[1][1] + (3 * t ** 3 - 5 * t ** 2 + 2) * point[2][1]
+                nowY = ((-t ** 3 + 2 * t ** 2 - t) * point[1][1] + (3 * t ** 3 - 5 * t ** 2 + 2) * point[2][1]
                          + (-3 * t ** 3 + 4 * t ** 2 + t) * point[3][1] + (t ** 3 - t ** 2) * point[4][1]) / 2
 
-                # draw p3-p4
-                for i in range(0, 100, 2):
-                    t = i / 100
-                    nowX = ((-t ** 3 + 2 * t ** 2 - t) * point[2][0] + (3 * t ** 3 - 5 * t ** 2 + 2) * point[3][0]
+            # draw p3-p4
+            for i in range(0, 100, 2):
+                t = i / 100
+                nowX = ((-t ** 3 + 2 * t ** 2 - t) * point[2][0] + (3 * t ** 3 - 5 * t ** 2 + 2) * point[3][0]
                          + (-3 * t ** 3 + 4 * t ** 2 + t) * point[4][0] + (t ** 3 - t ** 2) * point[1][0]) / 2
-                    nowY = ((-t ** 3 + 2 * t ** 2 - t) * point[2][1] + (3 * t ** 3 - 5 * t ** 2 + 2) * point[3][1]
+                nowY = ((-t ** 3 + 2 * t ** 2 - t) * point[2][1] + (3 * t ** 3 - 5 * t ** 2 + 2) * point[3][1]
                          + (-3 * t ** 3 + 4 * t ** 2 + t) * point[4][1] + (t ** 3 - t ** 2) * point[1][1]) / 2
 
-                # draw p4-p1
-                for i in range(0, 100, 2):
-                    t = i / 100
-                    nowX = ((-t ** 3 + 2 * t ** 2 - t) * point[3][0] + (3 * t ** 3 - 5 * t ** 2 + 2) * point[4][0]
+            # draw p4-p1
+            for i in range(0, 100, 2):
+                t = i / 100
+                nowX = ((-t ** 3 + 2 * t ** 2 - t) * point[3][0] + (3 * t ** 3 - 5 * t ** 2 + 2) * point[4][0]
                          + (-3 * t ** 3 + 4 * t ** 2 + t) * point[1][0] + (t ** 3 - t ** 2) * point[2][0]) / 2
-                    nowY = ((-t ** 3 + 2 * t ** 2 - t) * point[3][1] + (3 * t ** 3 - 5 * t ** 2 + 2) * point[4][1]
+                nowY = ((-t ** 3 + 2 * t ** 2 - t) * point[3][1] + (3 * t ** 3 - 5 * t ** 2 + 2) * point[4][1]
                          + (-3 * t ** 3 + 4 * t ** 2 + t) * point[1][1] + (t ** 3 - t ** 2) * point[2][1]) / 2
 
-    Doit = False
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 character = load_image('animation_sheet.png')
@@ -83,6 +80,7 @@ while running:
 
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+
     character.clip_draw(frame * 100, 100, 100, 100, nowX, nowY)
     update_canvas()
     frame = (frame + 1) % 8
