bb4db63f09d9e5fd4c2ad14dddcc0335cc9a9782
2018180013-2DGP <1057542@naver.com>
2019-10-09 18:28:32 +0900

    Update Drill-06.py

--- Drills/Drill-06/2번/Drill-06.py
+++ Drills/Drill-06/2번/Drill-06.py
@@ -10,13 +10,12 @@ def curve(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10):
         t = i / 100
         nowX = ((-t**3 + 2*t**2 - t)*p10[0] + (3*t**3 - 5*t**2 + 2)*p1[0] + (-3*t**3 + 4*t**2 + t)*p2[0] + (t**3 - t**2)*p3[0])/2
         nowY = ((-t**3 + 2*t**2 - t)*p10[1] + (3*t**3 - 5*t**2 + 2)*p1[1] + (-3*t**3 + 4*t**2 + t)*p2[1] + (t**3 - t**2)*p3[1])/2
-    character.clip_draw(frame * 100, 100, 100, 100, nowX, nowY)
+
     # draw p2-p3
     for i in range(0, 100, 2):
         t = i / 100
         nowX = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
         nowY = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
-    character.clip_draw(frame * 100, 100, 100, 100, nowX, nowY)
 
     # draw p3-p4
     for i in range(0, 100, 2):
@@ -93,13 +92,10 @@ while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     character.clip_draw(frame * 100, 100, 100, 100, nowX, nowY)
-    curve((random.randint(-400, 400), random.randint(-400, 400)), (random.randint(-400, 400), random.randint(-400, 400)),
-          (random.randint(-400, 400), random.randint(-400, 400)), (random.randint(-400, 400), random.randint(-400, 400)),
-          (random.randint(-400, 400), random.randint(-400, 400)), (random.randint(-400, 400), random.randint(-400, 400)),
-          (random.randint(-400, 400), random.randint(-400, 400)), (random.randint(-400, 400), random.randint(-400, 400)),
-          (random.randint(-400, 400), random.randint(-400, 400)), (random.randint(-400, 400), random.randint(-400, 400)))
+
     frame = (frame + 1) % 8
 
+    update_canvas()
     delay(0.02)
 
 close_canvas()
