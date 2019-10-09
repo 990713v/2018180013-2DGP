b49ca3057ea0bd4fcc67e49cab53478de7e5c999
2018180013-2DGP <1057542@naver.com>
2019-10-09 18:05:48 +0900

    실습
    실습

--- 실습/Lecture08_Curve_Movement/curve.py
+++ 실습/Lecture08_Curve_Movement/curve.py
@@ -54,8 +54,15 @@ def draw_point(p):
 
 
 def draw_curve_3_points(p1, p2, p3):
-    # fill here
-    pass
+    draw_big_point(p1)
+    draw_big_point(p2)
+    draw_big_point(p3)
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = (2 * t ** 2 - 3 * t + 1) * p1[0] + (-4 * t ** 2 + 4 * t) * p2[0] + (2 * t ** 2 - t) * p3[0]
+        y = (2 * t ** 2 - 3 * t + 1) * p1[1] + (-4 * t ** 2 + 4 * t) * p2[1] + (2 * t ** 2 - t) * p3[1]
+        draw_point((x, y))
+    draw_point(p3)
 
 
 def draw_curve_4_points(p1, p2, p3, p4):
