0ef27ae09ef53cc218b4fd8b3366ef3bead6d285
2018180013-2DGP <1057542@naver.com>
2019-10-21 18:48:59 +0900

    dr8

---
+++ Drills/Drill-08(1)/pause_state.py
@@ -0,0 +1,54 @@
+from pico2d import *
+
+import game_framework
+
+class Pause:
+    def __init__(self):
+        self.image = load_image('pause.png')
+        self.time = 0
+
+    def update(self):
+        self.time = 1 - self.time
+        delay(0.1)
+        pass
+
+    def draw(self):
+        self.image.draw(400, 300, 500, 500)
+
+
+def enter():
+    global pause
+    pause = Pause()
+
+
+def exit():
+    global pause
+    del pause
+
+
+def update():
+    global pause
+    pause.update()
+
+
+def draw():
+    global pause
+    clear_canvas()
+    pause.update()
+    pause.draw()
+    update_canvas()
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_KEYDOWN and event.key == SDLK_p:
+            game_framework.pop_state()
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
