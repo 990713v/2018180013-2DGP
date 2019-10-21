33f2c4ca618cbe3b6eec1ecdc8214d6d85c60193
2018180013-2DGP <1057542@naver.com>
2019-10-21 18:47:52 +0900

    dr8

--- Drills/Drill-08(1)/main_state.py
+++ Drills/Drill-08(1)/main_state.py
@@ -58,14 +58,6 @@ def exit():
     pass
 
 
-def pause():
-    pass
-
-
-def resume():
-    pass
-
-
 def handle_events():
     events = get_events()
     for event in events:
@@ -73,9 +65,10 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.change_state(title_state)
+
+            # p 키를 눌렀을 때 게임상태를 pause로 변화
         elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
             game_framework.push_state(pause_state)
-    pass
 
 
 def update():
