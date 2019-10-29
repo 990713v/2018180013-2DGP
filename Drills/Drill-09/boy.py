c34e8832c9616cc516c25656bd9f4ad878327430
2018180013-2DGP <1057542@naver.com>
2019-10-29 11:49:48 +0900

    dr9
    dr9

--- Drills/Drill-09/boy.py
+++ Drills/Drill-09/boy.py
@@ -1,16 +1,17 @@
 from pico2d import *
 
 # Boy Event
-RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP, SLEEP_TIMER = range(5)
+RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP, SHIFT_DOWN, SHIFT_UP, SLEEP_TIMER = range(7)
 
 key_event_table = {
     (SDL_KEYDOWN, SDLK_RIGHT): RIGHT_DOWN,
     (SDL_KEYDOWN, SDLK_LEFT): LEFT_DOWN,
+    (SDL_KEYDOWN, SDLK_LSHIFT): SHIFT_DOWN,
+
     (SDL_KEYUP, SDLK_RIGHT): RIGHT_UP,
-    (SDL_KEYUP, SDLK_LEFT): LEFT_UP
+    (SDL_KEYUP, SDLK_LEFT): LEFT_UP,
+    (SDL_KEYUP, SDLK_LSHIFT): SHIFT_UP
 }
-
-
 
 # Boy States
 class IdleState:
@@ -98,7 +99,34 @@ class SleepState:
         else: boy.image.clip_composite_draw(boy.frame * 100, 200, 100, 100,
             -3.141592 / 2, '', boy.x + 25, boy.y - 25, 100, 100)
 
+class DashState:
+    @staticmethod
+    def enter(boy, event):
+        if event == SHIFT_DOWN:
+            boy.velocity += 1
+        if event == SHIFT_UP:
+            boy.velocity -= 1
 
+    @staticmethod
+    def exit(boy, event):
+        pass
+
+    @staticmethod
+    def do(boy, event):
+        boy.frame = (boy.frame + 1) % 8
+        if event == SHIFT_DOWN:
+            boy.timer == 100
+        if event == SHIFT_UP:
+            boy.timer = 0
+
+        boy.x += boy.velocity
+        boy.x = clamp(25, boy.x, 800 - 25)
+
+    def draw(boy):
+        if boy.velocity == 1:
+            boy.image.clip_draw(boy.frame * 100, 100, 100, 100, boy.x, boy.y)
+        else:
+            boy.image.clip_draw(boy.frame * 100, 0, 100, 100, boy.x, boy.y)
 
 next_state_table = {
     IdleState: {RIGHT_UP: RunState, LEFT_UP: RunState,
@@ -106,6 +134,9 @@ next_state_table = {
                 SLEEP_TIMER: SleepState},
     RunState: {RIGHT_UP: IdleState, LEFT_UP: IdleState,
                LEFT_DOWN: IdleState, RIGHT_DOWN: IdleState},
+
+    DashState: {SHIFT_UP: DashState, SHIFT_DOWN: DashState},
+
     SleepState: {LEFT_DOWN: RunState, RIGHT_DOWN: RunState,
                  LEFT_UP: RunState, RIGHT_UP: RunState}
 
@@ -127,7 +158,7 @@ class Boy:
         pass
 
 
-    def change_state(self, state): #update_state
+    def update_state(self, state): #update_state
         if len(self.event_que) > 0:
             event = self.event_que.pop()
             self.cur_state.exit(self, event)
