1366532a3aa2d84511fb63282f74098a303fb15e
2018180013-2DGP <1057542@naver.com>
2019-11-07 14:45:43 +0900

    게임 개발
    오브젝트 파일 분할
    game world 이용

---
+++ project/pac/Player.py
@@ -0,0 +1,59 @@
+import game_world
+
+nowX, nowY = 0, 0
+
+class Player:
+    def __init__(self):
+        self.x, self.y = 162, 110
+        self.frame = 4
+        self.locate = 0
+        self.image = load_image('pacman.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 4
+        delay(0.1)
+
+    def handle_events(self, event):
+        global nowX, nowY
+        events = get_events()
+
+        for event in events:
+            if event.type == SDL_KEYDOWN:
+                if event.key == SDLK_RIGHT:
+                    nowX += 1
+                    nowY = 0
+                    self.locate = 1
+
+                elif event.key == SDLK_LEFT:
+                    nowX -= 1
+                    nowY = 0
+                    self.locate = 2
+
+                elif event.key == SDLK_UP:
+                    nowY += 1
+                    nowX = 0
+                    self.locate = 3
+
+                elif event.key == SDLK_DOWN:
+                    nowY -= 1
+                    nowX = 0
+                    self.locate = 4
+
+        self.x += nowX * 5
+        self.y += nowY * 5
+
+    def draw(self):
+        if self.locate <= 1: # 오른쪽
+            self.image.clip_draw(self.frame * 29, 0, 29, 16, self.x, self.y)
+        elif self.locate == 2: # 왼쪽
+            self.image.clip_draw(self.frame * 29, 16, 29, 16, self.x, self.y)
+        elif self.locate == 3: # 위쪽
+            self.image.clip_draw(self.frame * 29, 48, 29, 16, self.x, self.y)
+        elif self.locate == 4: # 아래쪽
+            self.image.clip_draw(self.frame * 29, 32, 29, 16, self.x, self.y)
+
+        # 범위체크 ( 값 수정해줄것)
+        #if self.x < 190:
+            #self.x = 310
+       # if self.x > 310:
+        #    self.x = 290
--- project/pac/main_state.py
+++ project/pac/main_state.py
@@ -3,13 +3,13 @@ import json
 import os
 
 from pico2d import *
-
 import game_framework
 import title_state
 
-os.chdir('resourse')
+from player import Player
 
 name = "MainState"
+os.chdir('resourse')
 
 game_map = None
 player = None
@@ -88,67 +88,6 @@ class Emy_Orange:
 
 
 
-
-nowX, nowY = 0, 0
-
-class Player:
-    def __init__(self):
-        self.x, self.y = 162, 110
-        self.frame = 4
-        self.locate = 0
-        self.image = load_image('pacman.png')
-
-    def update(self):
-        self.frame = (self.frame + 1) % 4
-        delay(0.1)
-
-    def handle_events(self, event):
-        global nowX, nowY
-        events = get_events()
-
-        for event in events:
-            if event.type == SDL_KEYDOWN:
-                if event.key == SDLK_RIGHT:
-                    nowX += 1
-                    nowY = 0
-                    self.locate = 1
-
-                elif event.key == SDLK_LEFT:
-                    nowX -= 1
-                    nowY = 0
-                    self.locate = 2
-
-                elif event.key == SDLK_UP:
-                    nowY += 1
-                    nowX = 0
-                    self.locate = 3
-
-                elif event.key == SDLK_DOWN:
-                    nowY -= 1
-                    nowX = 0
-                    self.locate = 4
-
-        self.x += nowX * 5
-        self.y += nowY * 5
-
-    def draw(self):
-        if self.locate <= 1: # 오른쪽
-            self.image.clip_draw(self.frame * 29, 0, 29, 16, self.x, self.y)
-        elif self.locate == 2: # 왼쪽
-            self.image.clip_draw(self.frame * 29, 16, 29, 16, self.x, self.y)
-        elif self.locate == 3: # 위쪽
-            self.image.clip_draw(self.frame * 29, 48, 29, 16, self.x, self.y)
-        elif self.locate == 4: # 아래쪽
-            self.image.clip_draw(self.frame * 29, 32, 29, 16, self.x, self.y)
-
-        # 범위체크 ( 값 수정해줄것)
-        #if self.x < 190:
-            #self.x = 310
-       # if self.x > 310:
-        #    self.x = 290
-
-
-
 def enter():
     global player, game_map
     global emy_Red, emy_Blue, emy_Pink, emy_Orange
@@ -164,44 +103,26 @@ def enter():
 
 
 def exit():
-    global player, game_map
-    global emy_Red, emy_Blue, emy_Pink, emy_Orange
-    
-    del(player)
-    del(game_map)
-    
-    del(emy_Red)
-    del(emy_Blue)
-    del(emy_Pink)
-    del(emy_Orange)
+    game_world.claer()
     pass
 
 
 def handle_events():
-   # events = get_events()
     player.handle_events(events)
     pass
 
 
 def update():
-    player.update()
-    
-    emy_Red.update()
-    emy_Blue.update()
-    emy_Pink.update()
-    emy_Orange.update()
+    for game_object in game_world.all_objects():
+        game_object.update()
     pass
 
 
 def draw():
     clear_canvas()
-    game_map.draw()
-    player.draw()
-    
-    emy_Red.draw()
-    emy_Blue.draw()
-    emy_Pink.draw()
-    emy_Orange.draw()
+
+    for game_object in game_world.all_objects():
+        game_object.draw()
     
     update_canvas()
     pass
