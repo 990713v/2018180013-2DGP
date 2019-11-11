793ffa3fc2700db10813bdb772622e56428c1543
2018180013-2DGP <1057542@naver.com>
2019-11-11 23:46:27 +0900

    게임개발ㅁ
    맵 리소스 수정

--- project/pac/Game_map.py
+++ project/pac/Game_map.py
@@ -3,13 +3,13 @@ import game_world
 
 class Game_map:
     def __init__(self):
-        self.image = load_image('coin_map.png')
+        # self.image = load_image('coin_map.png')
+        self.image = load_image('original_map.png')
         self.image2 = load_image('black.png')
-
+    
     def update(self):
         pass
 
-    def draw(self):
-        
+    def draw(self):        
         self.image2.draw(162, 174)
         self.image.draw(162, 174)
