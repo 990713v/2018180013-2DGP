20121b58b3fd21ab6cf7ccc3cd95c4866b107364
2018180013-2DGP <1057542@naver.com>
2019-11-12 23:05:05 +0900

    Drill 10
    Drill 10

---
+++ Drills/Drill-10/bird.py
@@ -0,0 +1,39 @@
+import game_framework
+from pico2d import *
+
+import game_world
+
+PIXEL_PER_METER = (10.0 / 0.3) # 10 pixel 30 cm
+RUN_SPEED_KMPH = 20.0 # km / hour
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
+
+class Bird:
+
+    def __init__(self):
+        self.x, self.y = 1600 // 2, 90       
+        self.image = load_image('bird_animation.png')
+        self.font = load_font('ENCR10B.TTF', 16)
+        self.dir = 1
+        self.velocity = 0
+        self.frame = 0
+
+    def update(self):
+        self.cur_state.do(self)
+        if len(self.event_que) > 0:
+            event = self.event_que.pop()
+            self.cur_state.exit(self, event)
+            self.cur_state = next_state_table[self.cur_state][event]
+            self.cur_state.enter(self, event)
+
+    def draw(self):
+        self.cur_state.draw(self)
+        self.font.draw(self.x - 60, self.y + 50, '(Time: %3.2f)' % get_time(), (255, 255, 0))
+        # get_time(): 시간 획득 함수. Canvas 열린 시점시간 0.0 부터 시작
+
