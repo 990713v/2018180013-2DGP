413b0b0eab90e078855750cff506481916ba61b2
2018180013-2DGP <1057542@naver.com>
2019-11-14 15:56:54 +0900

    파일수정작업
    파일수정작업

---
+++ project/pac/emy_Blue.py
@@ -0,0 +1,15 @@
+from pico2d import *
+import game_world
+
+class Emy_Blue:
+    def __init__(self):
+        self.x, self.y = 191, 180
+        self.frame = 4
+        self.image = load_image('blue.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 4
+
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 29, 32, 29, 16, self.x, self.y)
---
+++ project/pac/emy_Red.py
@@ -0,0 +1,15 @@
+from pico2d import *
+import game_world
+
+class Emy_Red:
+    def __init__(self):
+        self.x, self.y = 162, 180
+        self.frame = 4
+        self.image = load_image('red.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 4
+
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 29, 32, 29, 16, self.x, self.y)
